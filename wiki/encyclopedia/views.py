04b33db4b776ea0b7a6516ab00c087f4a314c6f3
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-04-09 13:08:45 +0500

    created wiki proj

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request,title):
+    html_content = convert_md_to_html(title)
+    if html_content == None:
+        return render(request,"encyclopedia/eror.html" , {'msg': 'This entry not exist ' })
+    else:
+        return render(request,"encyclopedia/entry.html" , {
+            'title': title,
+            'content': html_content
+        })
+
+def search(request):
+    if request.method == "POST":
+        entry_search = request.POST['q']
+        html_content = convert_md_to_html(entry_search)
+        if html_content is not None:
+            return render(request,"encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+            })
+        else:
+            alll = util.list_entries()
+            rec = []
+            for entry in alll:
+                if entry_search.lower() in entry.lower():
+                    rec.append(entry)
+            return render (request,"encyclopedia/search.html", {
+                'rec': rec
+            })
